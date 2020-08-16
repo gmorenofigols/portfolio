938598c4887ed4b667efd742495d79d6d6e6b5f4
Genis M <gmorenofigols@localhost.localdomain>
2020-08-17 00:56:30 +0200

    added DynamicHeader and db. Not working

--- src/App.js
+++ src/App.js
@@ -4,24 +4,32 @@ import Header from './components/Header'
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
+import axios from 'axios';
 
 const App = () => {
+
+  axios
+    .get('http://localhost:3001/notes')
+    .then( response => {
+      console.log(response.data);
+    })
+
   return (
     <div className = "main-wrapper">
       <Header className = "header" name = "header"/>
       <CssBaseline />
-      <Container disableGutters = "true" maxWidth ="false">
+      <Container disableGutters = {true} maxWidth = {false}>
         <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '50vh'}} />
       </Container>
       <CssBaseline />
-      <Container disableGutters = "true" maxWidth ="false">
+      <Container disableGutters = {true} maxWidth = {false}>
         <Typography component="div" style={{ backgroundColor: '#f000ff', height: '50vh'}} />
       </Container>
       <CssBaseline />
-      <Container disableGutters = "true" maxWidth ="false">
+      <Container disableGutters = {true} maxWidth = {false}>
         <Typography component="div" style={{ backgroundColor: '#123456', height: '50vh'}} />
       </Container>
-      <Container disableGutters = "true" maxWidth ="false">
+      <Container disableGutters = {true} maxWidth = {false}>
         <Typography component="div" style={{ backgroundColor: '#f000ff', height: '50vh'}} />
       </Container>
     </div>  
